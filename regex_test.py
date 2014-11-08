dc704c0e675d7b47a283f0ff88f7c4463ea7cb18
rvill <rvill@alum.mit.edu>
2014-11-08 17:24:57 -0500

    regex search on url list

--- regex_test.py
+++ regex_test.py
@@ -1,12 +1,7 @@
 import re
 
-#file=open('url.txt','r')
 
-#for line in  file:
-url='https://farm4.staticflickr.com/3943/15386021170_c2607cb639_z.jpg'
-
-result=re.search(r'^(https://)?(farm4\.)?(staticflickr\.com/)?(\d+)?(\D+)(\d+)',url).group(6)
+result =  re.search(r'^(https://)?(farm4\.)?(staticflickr\.com/)?(\d+)?(\D+)(\d+)',open('url.txt', 'r').read())
 
 print result
 
-#file.close()
