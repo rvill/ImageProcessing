24e8eb374f19b7573a8257177c10b4fd89340fe6
rvill <rvill@alum.mit.edu>
2014-11-11 15:39:44 -0500

    prints each character found as a list, character by character

--- regex_test.py
+++ regex_test.py
@@ -1,7 +1,9 @@
+#prints  each group as a separate line
 import re
 
-
-result =  re.search(r'^(https://)?(farm4\.)?(staticflickr\.com/)?(\d+)?(\D+)(\d+)',open('url.txt', 'r').read())
-
-print result
+file = open('url.txt')
+text = file.read()
+matchstr = re.findall(r'https://farm4.staticflickr.com?.\d+.d+',text)
+for image_ids in text:
+    print image_ids
 
