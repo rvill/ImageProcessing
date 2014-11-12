9428fbc651546070d59719db27f1cbf9003ccce1
rvill <rvill@alum.mit.edu>
2014-11-11 23:55:24 -0500

    added exception handling

--- regex_jhead.py
+++ regex_jhead.py
@@ -21,24 +21,26 @@ matches = re.findall('\d{10,11}',filetext)
       
 #2. download images from flickrapi with found ids
 for photo in matches:
-     url = flickr.photos_getSizes(photo_id=photo).getiterator('size')[-1].attrib['source']
+    try:
+         url = flickr.photos_getSizes(photo_id=photo).getiterator('size')[-1].attrib['source']
 
-     filename = '%s.'%photo + url.split('.')[-1].split('?')[0]
+         filename = '%s.'%photo + url.split('.')[-1].split('?')[0]
 
-     urllib.urlretrieve(url, filename)
+         urllib.urlretrieve(url, filename)
 
-     tags = flickr.photos_getExif(photo_id=photo).getiterator('exif')
+         tags = flickr.photos_getExif(photo_id=photo).getiterator('exif')
 
-     with open('tags.xml', 'w') as tags_file:
-         tags_file.write("<?xml version='1.0' encoding='UTF-8'?>\n<rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>\n")
-         for tag in tags:
-             tags_file.write('<%s:%s>%s</%s:%s>\n'%(tag.attrib['tagspace'], tag.attrib['tag'], tag.getchildren()[0].text.strip().encode('utf-8'), tag.attrib['tagspace'], tag.attrib['tag']))
-         tags_file.write('</rdf:RDF>\n')
+         with open('tags.xml', 'w') as tags_file:
+             tags_file.write("<?xml version='1.0' encoding='UTF-8'?>\n<rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>\n")
+             for tag in tags:
+                 tags_file.write('<%s:%s>%s</%s:%s>\n'%(tag.attrib['tagspace'], tag.attrib['tag'], tag.getchildren()[0].text.strip().encode('utf-8'), tag.attrib['tagspace'], tag.attrib['tag']))
+             tags_file.write('</rdf:RDF>\n')
 
-     os.system('exiftool -overwrite_original -tagsfromfile tags.xml %s'%filename)
-
-os.remove('tags.xml')
-
+         os.system('exiftool -overwrite_original -tagsfromfile tags.xml %s'%filename)
+      
+         os.remove('tags.xml')
+    except:
+        print "Can't go through. Permission denied by user"
 
 #3. jhead on each output
 
